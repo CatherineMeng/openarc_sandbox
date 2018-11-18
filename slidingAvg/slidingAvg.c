9f6dd3d0e081eba9ae5bdec17d9ec259ed83037a
CatherineMeng <myuan2009@gmail.com>
2018-11-18 13:28:27 -0600

    Re-format

--- slidingAvg/slidingAvg.c
+++ slidingAvg/slidingAvg.c
@@ -11,8 +11,6 @@ double get_wall_time(){
   return (double)time.tv_sec + (double)time.tv_usec * .000001;
 }
 
-//takes in a number n, an array a, return the average over
-//the range of 5
 void slidingAvg_host(int n, int* a, int* result) {
   int i;
   result[0]=result[1]=result[n-2]=result[n-1]=0;
@@ -25,9 +23,7 @@ void slidingAvg_host(int n, int* a, int* result) {
 
 void slidingAvg(int n, int* a, int* result) {
   int i;
-  //Create a local shift-register array with length of 5
   int shreg[5];
-  //Initialize the array as one unit before entering the input array
   shreg[0]=shreg[1]=0;
   shreg[2]=1; shreg[3]=2; shreg[4]=3;
   assert(n>0);
@@ -35,13 +31,11 @@ void slidingAvg(int n, int* a, int* result) {
   #pragma acc parallel num_gangs(1) num_workers(1) copyin(a[n]) copy(result[n])
   {
     #pragma acc loop seq 
-    //calculate sliding window average using shreg and write results to result array
     for(i=2; i<n-2; i++) {
       for (int ind=0; ind<4; ind++){
-        //shift by one unit
         shreg[ind]=shreg[ind+1];
       }
-      shreg[5]=a[i];
+      shreg[4]=a[i+2];
       result[i]=(shreg[0]+shreg[1]+shreg[2]+shreg[3]+shreg[4])/5;
     }
   }
@@ -53,10 +47,6 @@ int main() {
   int* result; 
   int* result2;
   double t0, t1, t2;
-//  n=2;
-//  for (int i=0; i<29; i++){
-//    n*=2;
-//  }
   n=10*2<<20;
   a = (int*) malloc(n*sizeof(int));
   result = (int*) malloc(n*sizeof(int));
@@ -73,12 +63,11 @@ int main() {
   t1 = get_wall_time();
   slidingAvg_host(n,a,result2);
   t2 = get_wall_time();
-  //compare the result arrays!
   for (i=0;i<n;i++){
     if (result[i]!=result2[i]){
-      printf("Error device result!=host result at index %d\r\n", i);
-      printf("Device result: %d\r\n",result[i]);
-      printf("Host result: %d\r\n", result2[i]);
+      printf("Error device result!=host result at index %d\n", i);
+      printf("Device result: %d\n",result[i]);
+      printf("Host result: %d\n", result2[i]);
       return 1;
     }
   }
